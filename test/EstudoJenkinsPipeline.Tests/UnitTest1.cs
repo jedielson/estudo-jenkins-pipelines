0869b69296b1faf138cdfe8f4a314f3a5ca12adc
Jedielson Nakonieczni <jedi_matind@hotmail.com>
2019-01-26 16:55:34 -0200

    Commit inicial

---
+++ test/EstudoJenkinsPipeline.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace EstudoJenkinsPipeline.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
